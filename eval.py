baeae4cde0e5991dd86d87135691b50d64b74552
Caesar-s1mple <1023874067@qq.com>
2022-10-01 12:43:18 +0800

    no message

---
+++ eval.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import argparse
+
+
+def micro_f1(pred, label, neg):
+    total = len(pred)
+    correct_positive = 0
+    pred_positive = 0
+    gold_positive = 0
+    for i in range(total):
+        golden = label[i]
+        if golden == pred[i]:
+            if golden != neg:
+                correct_positive += 1
+        if golden != neg:
+            gold_positive += 1
+        if pred[i] != neg:
+            pred_positive += 1
+    try:
+        micro_p = float(correct_positive) / float(pred_positive)
+    except:
+        micro_p = 0
+    try:
+        micro_r = float(correct_positive) / float(gold_positive)
+    except:
+        micro_r = 0
+    try:
+        micro_f1 = 2 * micro_p * micro_r / (micro_p + micro_r)
+    except:
+        micro_f1 = 0
+    result = {'micro_p': micro_p, 'micro_r': micro_r, 'micro_f1': micro_f1}
+    return result
+
+
+
+def parse_():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--test-file', type=str, default='./data/kgpool/kgpool_wiki20m_result.csv')
+    parser.add_argument('--NA', type=str, default='NA')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_()
+
+    result = pd.read_csv(args.test_file, na_filter=None, dtype=str)
+    label = result.iloc[:, 2]
+    pred = result.iloc[:, 3]
+
+    res = micro_f1(pred, label, args.NA)
+
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
